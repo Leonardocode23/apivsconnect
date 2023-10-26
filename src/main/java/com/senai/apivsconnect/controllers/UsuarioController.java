ad22fee25566ad9f64ab3cccccb7652a70f712f5
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-10-25 21:10:25 -0300

    teste

---
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.senai.apivsconnect.controllers;
+
+import com.senai.apivsconnect.models.UsuarioModel;
+import com.senai.apivsconnect.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/usuarios", produces = {"application/json"})
+public class UsuarioController {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>>listarUsuarios(){
+        //Retorna response com a lista de usuarios
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
+
+}
