adadea7653a89d3b877669eddc6ede66e771d1f3
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-10-25 21:43:31 -0300

    teste2

--- src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value="/usuarios", produces = {"application/json"})
@@ -22,5 +25,16 @@ public class UsuarioController {
         //Retorna response com a lista de usuarios
         return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> exibirUsuario(@PathVariable(value = "id")UUID id){
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
 
+        if (usuarioBuscado.isEmpty()){
+            // Retorna usuario nao encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario nao encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioBuscado.get());
+
+    }
 }
